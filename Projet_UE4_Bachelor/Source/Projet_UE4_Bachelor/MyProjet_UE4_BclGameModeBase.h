c08e60ee138918d30dce6f8bb6af218c2638f02f
TartifletteVEVO <56391305+TartifletteVEVO@users.noreply.github.com>
2019-10-17 14:34:47 +0200

    add content

---
+++ Projet_UE4_Bachelor/Source/Projet_UE4_Bachelor/MyProjet_UE4_BclGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Projet_UE4_BachelorGameModeBase.h"
+#include "MyProjet_UE4_BclGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET_UE4_BACHELOR_API AMyProjet_UE4_BclGameModeBase : public AProjet_UE4_BachelorGameModeBase
+{
+	GENERATED_BODY()
+	
+};
